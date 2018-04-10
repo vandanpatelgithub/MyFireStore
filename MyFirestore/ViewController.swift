1edb1d4ff16f5637a6a35b7f61b4d3c0ab1825db
Vandan Patel <vandan.patel@nike.com>
2018-04-09 19:50:26 -0700

    Initial Commit

---
+++ MyFirestore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirestore
+//
+//  Created by Patel, Vandan (ETW - FLEX) on 4/9/18.
+//  Copyright © 2018 Patel, Vandan (ETW - FLEX). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
